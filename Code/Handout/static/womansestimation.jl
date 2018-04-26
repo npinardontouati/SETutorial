411728cd3f563d156ddc44b4a8e69d2f2c9b7fe6
npinardontouati <37663110+npinardontouati@users.noreply.github.com>
2018-04-26 16:11:32 +0200

    Update womansestimation.jl

--- Code/Handout/static/womansestimation.jl
+++ Code/Handout/static/womansestimation.jl
@@ -56,8 +56,8 @@ end
 theta0 = [.5, log(.4), .4, .8, log(1), -log((2*.4*1)/(.3+.4*1) -1)]
 
 # display optimization
-optimal = optimize(llks, [theta0], method = :nelder_mead)
-mles = optimal.minimum
+optimal = optimize(llks, theta0, method = :nelder_mead)
+mles = optimal.minimizer
 mles[6] = (1/(1 + exp(-mles[6])) - .5)*2*exp(mles[2])*exp(mles[5])
 mles[2] = exp(mles[2])
 mles[5] = exp(mles[5])
